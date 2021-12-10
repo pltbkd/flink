d781a22b57ef4b11ea71172b13a76c86e8b2b039
Arvid Heise <arvid@ververica.com>
2021-12-10 16:11:17 +0100

    [poc] Add decomposed Sink V2 interface.
    The new interface separates concerns and will make future refactorings and extensions easier. The user immediately which methods needs to be implemented.

--- flink-core/src/main/java/org/apache/flink/util/InitContextInitializationContextAdapter.java
+++ flink-core/src/main/java/org/apache/flink/util/InitContextInitializationContextAdapter.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +16,11 @@
  * limitations under the License.
  */
 
-package org.apache.flink.api.connector.sink;
+package org.apache.flink.util;
 
 import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.connector.sink.Sink;
 import org.apache.flink.metrics.MetricGroup;
-import org.apache.flink.util.UserCodeClassLoader;
 
 import java.util.function.Supplier;
 
