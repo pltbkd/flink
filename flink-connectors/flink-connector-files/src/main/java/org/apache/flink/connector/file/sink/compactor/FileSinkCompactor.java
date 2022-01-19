e1a863e79da170872e7e94b783442cfcff54b1ec
luogen.lg <luogen.lg@alibaba-inc.com>
2022-01-19 17:51:16 +0800

    commit before compact

--- flink-connectors/flink-connector-files/src/main/java/org/apache/flink/connector/file/sink/compactor/FileSinkCompactor.java
+++ flink-connectors/flink-connector-files/src/main/java/org/apache/flink/connector/file/sink/compactor/FileSinkCompactor.java
@@ -3,16 +3,20 @@ package org.apache.flink.connector.file.sink.compactor;
 import org.apache.flink.connector.file.sink.FileSinkCommittable;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.functions.sink.filesystem.BucketWriter;
-import org.apache.flink.streaming.api.functions.sink.filesystem.InProgressFileWriter.PendingFileRecoverable;
 import org.apache.flink.streaming.api.functions.sink.filesystem.CompactingFileWriter;
+import org.apache.flink.streaming.api.functions.sink.filesystem.InProgressFileWriter.PendingFileRecoverable;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileSinkCompactor<InputT>
         implements Compactor<FileSinkCommittable, FileCompactRequest> {
     private final FileCompactor fileCompactor;
     private final BucketWriter<InputT, String> bucketWriter;
+    private boolean commitBeforeCompact = false;
 
     public FileSinkCompactor(
             FileCompactor fileCompactor, BucketWriter<InputT, String> bucketWriter) {
@@ -23,7 +27,7 @@ public class FileSinkCompactor<InputT>
     @Override
     public Iterable<FileSinkCommittable> compact(FileCompactRequest request) throws Exception {
         List<FileSinkCommittable> results = new ArrayList<>();
-        List<Path> compactingFiles = new ArrayList<>();
+        List<FileSinkCommittable> compactingCommittables = new ArrayList<>();
 
         for (FileSinkCommittable committable : request.getCommittable()) {
             if (committable.hasInProgressFileToCleanup()) {
@@ -33,10 +37,16 @@ public class FileSinkCompactor<InputT>
             }
 
             if (committable.hasPendingFile()) {
-                compactingFiles.add(FileCompactorUtil.getPath(committable.getPendingFile()));
+                compactingCommittables.add(committable);
             }
         }
 
+        preCompact(request, compactingCommittables, results);
+
+        List<Path> compactingFiles =
+                compactingCommittables.stream()
+                        .map(c -> FileCompactorUtil.getPath(c.getPendingFile()))
+                        .collect(Collectors.toList());
         PendingFileRecoverable compactedPendingFile = doCompact(request, compactingFiles);
         FileSinkCommittable compacted =
                 new FileSinkCommittable(request.getBucketId(), compactedPendingFile);
@@ -44,6 +54,30 @@ public class FileSinkCompactor<InputT>
 
         // TODO add compacted pending files to remove
         return results;
+    }
+
+    private void preCompact(
+            FileCompactRequest request,
+            List<FileSinkCommittable> compactingCommittables,
+            List<FileSinkCommittable> results)
+            throws IOException {
+        if (!commitBeforeCompact) {
+            return;
+        }
+
+        Iterator<FileSinkCommittable> iter = compactingCommittables.iterator();
+        while (iter.hasNext()) {
+            FileSinkCommittable committable = iter.next();
+            if (!FileCompactorUtil.getPath(committable.getPendingFile())
+                    .getName()
+                    .startsWith("..")) {
+                // pass through
+                results.add(committable);
+                iter.remove();
+            } else {
+                bucketWriter.recoverPendingFile(committable.getPendingFile()).commitAfterRecovery();
+            }
+        }
     }
 
     private PendingFileRecoverable doCompact(FileCompactRequest request, List<Path> compactingFiles)
